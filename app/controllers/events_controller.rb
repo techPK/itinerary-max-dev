94d0eebad4ce4f1bff181a198bfacf5c101f6d76
Percy Keeley <percy.keeley@softwareforest.com>
2013-07-16 19:33:02 -0400

    Add new search criteria to index webpage: where and when

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,7 +3,21 @@ class EventsController < ApplicationController
   # GET /events.json
   def index
     Event.update_all_events(5) unless Event.where(:appointed_start => DateTime.now.beginning_of_day..DateTime.now.end_of_day).present?
-    puts "*********#{params}********"
+    
+    puts "params='#{params.inspect}'"  # Testing only
+    # Use Session Variable to determine state
+    if session[:search].blank?
+      @where = '' # NYC' # "Manhattan Bronx Brooklyn Queens Staten-Island"
+      @when_day = '' #[any_day] today tomorrow beyond_tomorrow"
+      @when_time = '' # "[any_time] morning afternoon evening night late-night early-morning"
+      @admission = "[any] general frugal free senior student citypass nypass ny_explorer-pass go-select"
+    end
+
+    @where = params[:where] if params[:where].present?
+    @when_day = params[:when_day] if params[:when_day]
+    @when_time = params[:when_time] if params[:when_time]
+    @specials = params[:admission] if params[:admission]
+
     if @selected_categories = params[:selected_categories]
        @select_categories = @selected_categories.keys
       @events = Event.where(taxonomy:@selected_categories.keys)
